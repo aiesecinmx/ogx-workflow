e0027623682f0d74ca158b8f5d29a824dd3d3979
Sergio Garcia <sgs.garcia93@gmail.com>
2021-02-06 14:45:18 -0600

    refactor: add logging to joi validation pipe

--- src/common/pipes/joi-validation.pipe.ts
+++ src/common/pipes/joi-validation.pipe.ts
@@ -1,8 +1,15 @@
-import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
+import {
+  PipeTransform,
+  Injectable,
+  BadRequestException,
+  Logger,
+} from '@nestjs/common';
 import { ObjectSchema } from 'joi';
 
 @Injectable()
 export class JoiValidationPipe<T> implements PipeTransform {
+  private readonly logger = new Logger(JoiValidationPipe.name);
+
   constructor(private readonly schema: ObjectSchema<T>) {}
 
   async transform(value: any): Promise<T> {
@@ -13,7 +20,17 @@ export class JoiValidationPipe<T> implements PipeTransform {
       });
       return value;
     } catch (error) {
+      this.logger.error(`Validation error: ${error.message}`);
+      this.logger.error(this.deletePII(value));
       throw new BadRequestException(error.details, 'Validation Failed');
     }
+  }
+
+  private deletePII(person) {
+    delete person.password;
+    delete person.lastName;
+    delete person.email;
+    delete person.phone;
+    return person;
   }
 }
